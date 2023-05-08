87999887563f3d75c7ab08a013671770d7b67df6
Kevin Beier <beierkevin2002@outlook.com>
2023-05-08 23:08:10 +0200

    STEP: Removing temps

--- src/classes/Customer.java
+++ src/classes/Customer.java
@@ -20,24 +20,39 @@ public class Customer {
     };
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental rental = (Rental) enum_rentals.nextElement();
-            // add frequent renter points
-            frequentRenterPoints += rental.getFrequentRenterPoints();
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + "\t" + rental.getDaysRented() + "\t"
                     + String.valueOf(rental.getCharge()) + "\n";
-            totalAmount += rental.getCharge();
         }
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
+    }
+
+    private double getTotalCharge() {
+        double charge = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental rental = (Rental) enum_rentals.nextElement();
+            charge += rental.getCharge();
+        }
+        return charge;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int points = 0;
+        Enumeration enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental rental = (Rental) enum_rentals.nextElement();
+            points += rental.getFrequentRenterPoints();
+        }
+        return points;
     }
 }
